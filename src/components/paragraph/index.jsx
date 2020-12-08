0530bd1d1de29ccb601d88cf13731e7fdcba3e18
Tilman Miraß <mail@tlmn.io>
2020-12-08 09:42:07 +0100

    dangerouslySetInnerHTML on div

--- src/components/paragraph/index.jsx
+++ src/components/paragraph/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import style from './style';
 
 export default ({ children, ...attrs }) => (
   <p {...attrs}>
     <style jsx>{style}</style>
-    {children}
+    <div dangerouslySetInnerHTML={{ __html: children }} />
   </p>
 );
